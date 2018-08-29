86b6d3dce42bb5ec2c585c51fb3b4229be71d604
Bobi NGO <bngo@student.42.fr>
2018-08-29 17:57:14 +0200

    Removing server file + updating tweets feed

--- js/tweets.js
+++ js/tweets.js
@@ -1,57 +1,49 @@
 let group = document.querySelector(".tweet-feed");
 let datas = [];
-let tweetarr = [];
-let index = 0;
-
 let currentId = 0;
-setInterval(() => {
-  request('GET', 'https://manocf-server.herokuapp.com/hashtag', { currentId, tag : 'arethafranklin' })
-    .then(res => {
-      let data = JSON.parse(res);
-      let tweets = data.statuses;
 
-      tweets.forEach(tweet => {
-        if (!tweet.retweeted_status){
-          let exists = false;
-          datas.forEach(t => {
-            if (t.id == tweet.id){
-              exists = true;
-            }
-          })
-          if (!exists){
+setInterval(update, 1000);
+setInterval(UpdateBlock, 2500);
+
+function update() {
+  if (datas.length == 0) {
+    retrieveTweets();
+  }
+}
+
+function retrieveTweets(){
+  request('GET', 'https://tweets-server.herokuapp.com/tweets')
+      .then(res => {
+        let tweets = JSON.parse(res);
+
+        tweets.forEach(tweet => {
+          if (tweet){
             datas.push(tweet);
           }
-        }
+        });
+      })
+      .catch(err => {
+        console.log(err);
       });
-    })
-    .catch(() => {
-      console.log("Oups, une erreur");
-    });
+}
 
-    if (group.childElementCount == 0){
-      for(let i = 0; i < 5; i++){
-        addingBlock();
-      };
-    } else {
-      addingBlock();
-    }
-
-
-}, 2500);
-
-function addingBlock(){
-  if (group.childElementCount > 0 && document.querySelector('.tweet-loader')){
-    document.querySelector('.tweet-loader').remove();
-    document.querySelector('.tweetarrow').classList.remove('hidden');
-  }
+function UpdateBlock(){
   if (group.childElementCount >= 5 && group.children[0]){
     group.removeChild(group.children[0]);
   }
   
   if (datas[0]){
-    datas.shift();
-    group.appendChild(createBox(datas[0]));
     currentId = datas[0].id;
+    let box = createBox(datas[0]);
+    if (box){
+      group.appendChild(box);
+    }
+    datas.shift();
+  }
+  
+  if (group.childElementCount > 0 && document.querySelector('.tweet-loader')){
+    document.querySelector('.tweet-loader').remove();
+    document.querySelector('.tweetarrow').classList.remove('hidden');
   }
 }
 
@@ -83,7 +75,7 @@ function serialize(obj) {
 }
 
 function createBox(tweet){
-  let item = document.createElement('div'),
+  let item = document.createElement('a'),
   imageDiv = document.createElement('div'),
   img = document.createElement('img'),
   content = document.createElement('div'),
@@ -95,23 +87,26 @@ function createBox(tweet){
   item.style.backgroundColor = "#ffffff40";
   item.style.borderRadius = "4px";
   item.style.padding = "2%";
+  item.href = "https://twitter.com/statuses/" + tweet.status_id;
+  item.target = "_blank";
+  item.alt = "Tweet de " + tweet.username;
   item.appendChild(imageDiv);
   item.appendChild(content);
 
   imageDiv.classList.add('ui', 'tiny', 'image');
   imageDiv.appendChild(img);
-  img.src = (tweet.user.profile_image_url) ? tweet.user.profile_image_url : 'img/child_focus.png';
+  img.src = tweet.avatar;
 
   content.classList.add('content');
   content.appendChild(header);
   content.appendChild(textDiv);
 
   header.classList.add('header');
-  header.innerHTML = "@" + (tweet.user.screen_name ? tweet.user.screen_name :  null);
+  header.innerHTML = "@" + tweet.username;
 
   textDiv.classList.add('description');
   textDiv.appendChild(text);
 
-  text.innerHTML = tweet.full_text;
+  text.innerHTML = tweet.text;
   return item;
 }
